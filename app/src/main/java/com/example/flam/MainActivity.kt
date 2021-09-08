7ea7922589850730a4c6e7568e3482cb8826038b
Jekis11 <kogyxar94@gmail.com>
2021-09-08 12:35:43 +0300

    Add MainActiv

--- app/src/main/java/com/example/flam/MainActivity.kt
+++ app/src/main/java/com/example/flam/MainActivity.kt
@@ -1,16 +1,32 @@
 package com.example.flam
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
+class MainActivity : AppCompatActivity() {
 
- class MainActivity : AppCompatActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        buttonlogin.setOnClickListener {
+            val intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        buttonregister.setOnClickListener {
+            val intent = Intent(this,RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+        createnewaccount.setOnClickListener {
+            val intent = Intent(this,RegisterActivity::class.java)
+            startActivity(intent)
+        }
 
     }
  }
