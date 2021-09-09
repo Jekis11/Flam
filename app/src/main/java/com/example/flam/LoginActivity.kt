790986720b921e36cb782ad72812d07c74de5e66
Jekis11 <kogyxar94@gmail.com>
2021-09-09 15:54:46 +0300

    Add build

--- app/src/main/java/com/example/flam/LoginActivity.kt
+++ app/src/main/java/com/example/flam/LoginActivity.kt
@@ -1,11 +1,29 @@
 package com.example.flam
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        forgetpassword.setOnClickListener {
+            val intent = Intent(this,ForgotPasswordActivity::class.java)
+            startActivity(intent)
+        }
+
+        signin.setOnClickListener {
+            val intent = Intent(this,RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+        buttonloginandere.setOnClickListener {
+            val intent = Intent(this,OtherMethodsActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
