39b81fa2f1d20db1e3c15ac455ae6c3e93ab7b4b
Jekis11 <kogyxar94@gmail.com>
2021-09-10 14:07:08 +0300

    Add build

--- app/src/main/java/com/example/flam/LoginActivity.kt
+++ app/src/main/java/com/example/flam/LoginActivity.kt
@@ -1,30 +1,27 @@
 package com.example.flam
 
-import android.app.ProgressDialog
-import android.content.Intent
-import android.media.tv.TvContract
+import  android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PatternMatcher
-import android.text.TextUtils
 import android.util.Patterns
 import android.widget.Toast
-import androidx.appcompat.app.ActionBar
-import com.example.flam.databinding.ActivityLoginBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
 
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        auth = FirebaseAuth.getInstance()
 
         buttonlogin.setOnClickListener {
-            val intent = Intent(this, ExitLoginActivity::class.java)
-            startActivity(intent)
+            doLogin()
         }
 
         forgetpassword.setOnClickListener {
@@ -42,6 +39,63 @@ class LoginActivity : AppCompatActivity() {
             startActivity(intent)
         }
     }
+
+    private fun doLogin() {
+        if (emaillogin.text.toString().isEmpty()) {
+            emaillogin.error = "Please enter email"
+            emaillogin.requestFocus()
+            return
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(emaillogin.text.toString()).matches()) {
+            emaillogin.error = "Please enter valid email"
+            emaillogin.requestFocus()
+            return
+        }
+
+        if (passwordlogin.text.toString().isEmpty()) {
+            passwordlogin.error = "Please enter password"
+            passwordlogin.requestFocus()
+            return
+        }
+            auth.signInWithEmailAndPassword(emaillogin.text.toString(), passwordlogin.text.toString())
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        val user = auth.currentUser
+                        updateUI(user)
+                    } else {
+
+                        updateUI(null)
+                    }
+                }
+    }
+    public override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        updateUI(currentUser)
+    }
+
+    private fun updateUI(currentUser: FirebaseUser?) {
+
+        if (currentUser != null) {
+            if(currentUser.isEmailVerified) {
+                startActivity(Intent(this, ExitLoginActivity::class.java))
+                finish()
+            }else{
+                Toast.makeText(
+                    baseContext, "Please verify your email address.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        } else {
+            Toast.makeText(
+                baseContext, "Login failed.",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+
 }
 
 
