5b8bd360908eb1159eb4c573e7e7508ce531c647
Jekis11 <kogyxar94@gmail.com>
2021-09-10 11:29:59 +0300

    Add build

--- app/src/main/java/com/example/flam/RegisterActivity.kt
+++ app/src/main/java/com/example/flam/RegisterActivity.kt
@@ -1,11 +1,19 @@
 package com.example.flam
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        signin.setOnClickListener {
+            val intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
