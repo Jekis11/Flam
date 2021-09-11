8313e243351bf51ecc3206d7c0f233ae1e20edfd
Jekis11 <kogyxar94@gmail.com>
2021-09-12 02:39:36 +0300

    Add build

--- app/src/main/java/com/example/flam/LoginActivity.kt
+++ app/src/main/java/com/example/flam/LoginActivity.kt
@@ -1,10 +1,14 @@
 package com.example.flam
 
+import android.app.Activity
 import  android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.util.Patterns
 import android.widget.Toast
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.activity_login.*
@@ -12,13 +16,13 @@ import kotlinx.android.synthetic.main.activity_login.*
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
-
+    private val TAG = LoginActivity::class.qualifiedName
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        auth = FirebaseAuth.getInstance()
+       val auth = FirebaseAuth.getInstance()
 
         buttonlogin.setOnClickListener {
             doLogin()
@@ -29,10 +33,6 @@ class LoginActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        buttonloginandere.setOnClickListener {
-            val intent = Intent(this,OtherMethodsActivity::class.java)
-            startActivity(intent)
-        }
 
         signup.setOnClickListener {
             val intent = Intent(this,RegisterActivity::class.java)
@@ -93,10 +93,48 @@ class LoginActivity : AppCompatActivity() {
                 Toast.LENGTH_SHORT
             ).show()
         }
+        if(auth.currentUser != null){
+            val intent = Intent(this,ExitLoginActivity::class.java)
+            intent.putExtra(USER_ID, auth.currentUser!!.uid)
+            startActivity(intent)
+        }
+        buttonloginandere.setOnClickListener {
+
+            val providers = arrayListOf(
+                AuthUI.IdpConfig.EmailBuilder().build()
+            )
+
+            startActivityForResult(
+                AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build(),
+                RC_SIGN_IN)
+        }
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == RC_SIGN_IN){
+            val response = IdpResponse.fromResultIntent(data)
 
-}
+            if(resultCode == Activity.RESULT_OK){
+                val user = FirebaseAuth.getInstance().currentUser
+                val intent = Intent(this,ExitLoginActivity::class.java)
+                intent.putExtra(USER_ID, user!!.uid)
+                startActivity(intent)
+            }
+            else{
+                Log.e(TAG,"Sign-in failed", response!!.error)
+            }
+        }
+    }
+    companion object {
+        const val USER_ID="user_id"
+        const val RC_SIGN_IN = 15
+    }
+    }
+
 
 
 
