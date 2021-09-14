c3eef091586a97fb516f3763e0d73a01c2d6abae
Jekis11 <kogyxar94@gmail.com>
2021-09-14 15:34:40 +0300

    Add build

--- app/src/main/java/com/example/flam/HauptActivity.kt
+++ app/src/main/java/com/example/flam/HauptActivity.kt
@@ -3,9 +3,9 @@ package com.example.flam
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class ExitLoginActivity : AppCompatActivity() {
+class HauptActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_exit_login)
+        setContentView(R.layout.activity_haupt)
     }
 }
--- app/src/main/java/com/example/flam/LoginActivity.kt
+++ app/src/main/java/com/example/flam/LoginActivity.kt
@@ -1,12 +1,22 @@
 package com.example.flam
 
 import android.app.Activity
+import android.app.KeyguardManager
+import android.content.Context
+import android.content.DialogInterface
 import  android.content.Intent
+import android.content.pm.PackageManager
+import android.hardware.biometrics.BiometricPrompt
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CancellationSignal
 import android.util.Log
 import android.util.Patterns
 import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
+import bolts.Bolts
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
@@ -19,13 +29,46 @@ class LoginActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
     private val TAG = MainActivity::class.qualifiedName
+    private var cancellationSignal: CancellationSignal? = null
+    private val authenticationCallback: BiometricPrompt.AuthenticationCallback
+    get() =
+        @RequiresApi(Build.VERSION_CODES.P)
+        object : BiometricPrompt.AuthenticationCallback(){
+            override fun onAuthenticationError(errorCode: Int, errString: CharSequence?) {
+                super.onAuthenticationError(errorCode, errString)
+                notifyUser("Authentication error: $errString")
+            }
 
+            override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult?) {
+                super.onAuthenticationSucceeded(result)
+                notifyUser("Authentication success!")
+                startActivity(Intent(this@LoginActivity,HauptActivity::class.java))
+            }
+        }
+
+    private fun notifyUser(message: String){
+        Toast.makeText(this,message, Toast.LENGTH_SHORT).show()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.P)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         auth = FirebaseAuth.getInstance()
         val autr = FirebaseAuth.getInstance()
 
+        checkBiometricSupport()
+
+        buttonfinger.setOnClickListener{
+            val biometricPrompt:BiometricPrompt  = BiometricPrompt.Builder(this)
+                .setTitle("Title of prompt")
+                .setSubtitle("Authentication is reguired")
+                .setDescription("This app uses fingerprint protection to keep your data secure")
+                .setNegativeButton("Cancel",this.mainExecutor, DialogInterface.OnClickListener{dialog, which ->
+                    notifyUser("Authentication cancelled")
+                }).build()
+            biometricPrompt.authenticate(getCancellationSignal(),mainExecutor,authenticationCallback)
+        }
 
 
         buttonlogin.setOnClickListener {
@@ -43,6 +86,32 @@ class LoginActivity : AppCompatActivity() {
             startActivity(intent)
         }
     }
+
+    private fun getCancellationSignal() : CancellationSignal {
+        cancellationSignal = CancellationSignal()
+        cancellationSignal?.setOnCancelListener {
+            notifyUser("Authentication was cancelled by the user")
+        }
+        return cancellationSignal as CancellationSignal
+    }
+
+    private fun checkBiometricSupport() : Boolean {
+        val keyguardManager:KeyguardManager = getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager
+        if(!keyguardManager.isKeyguardSecure){
+            notifyUser("Fingerprint authentication han not been enabled in settings")
+            return false
+        }
+        if (ActivityCompat.checkSelfPermission(this,android.Manifest.permission.USE_BIOMETRIC)!= PackageManager.PERMISSION_GRANTED){
+            notifyUser("Fingerprint authentication permission is not enabled")
+            return false
+        }
+        return if(packageManager.hasSystemFeature(PackageManager.FEATURE_FINGERPRINT)){
+            true
+        }
+        else true
+    }
+
+
 
     private fun doLogin() {
         if (emaillogin.text.toString().isEmpty()) {
@@ -83,7 +152,7 @@ class LoginActivity : AppCompatActivity() {
 
         if (currentUser != null) {
             if(currentUser.isEmailVerified) {
-                startActivity(Intent(this, ExitLoginActivity::class.java))
+                startActivity(Intent(this, HauptActivity::class.java))
                 finish()
             }else{
                 Toast.makeText(
@@ -97,6 +166,9 @@ class LoginActivity : AppCompatActivity() {
                 Toast.LENGTH_SHORT
             ).show()
         }
+
+
+
 
 
         buttonloginandere.setOnClickListener {
@@ -127,7 +199,7 @@ class LoginActivity : AppCompatActivity() {
 
             if (resultCode == Activity.RESULT_OK) {
                 val user = FirebaseAuth.getInstance().currentUser
-                val intent = Intent(this, ExitLoginActivity::class.java)
+                val intent = Intent(this, HauptActivity::class.java)
                 intent.putExtra(USER_ID, user!!.uid)
                 startActivity(intent)
             } else {
