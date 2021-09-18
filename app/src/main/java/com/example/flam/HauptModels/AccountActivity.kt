513afb95a5d1521fb0b85e0a734d443c1566052e
Jekis11 <kogyxar94@gmail.com>
2021-09-18 15:23:20 +0300

    Add build

---
+++ app/src/main/java/com/example/flam/HauptModels/AccountActivity.kt
@@ -0,0 +1,12 @@
+package com.example.flam.HauptModels
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.flam.R
+
+class AccountActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_account)
+    }
+}
