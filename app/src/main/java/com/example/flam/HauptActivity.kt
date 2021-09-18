a59d233b758c781d361feacebe469e0975b7d207
Jekis11 <kogyxar94@gmail.com>
2021-09-19 00:15:40 +0300

    Add build

--- app/src/main/java/com/example/flam/HauptActivity.kt
+++ app/src/main/java/com/example/flam/HauptActivity.kt
@@ -1,28 +1,25 @@
 package com.example.flam
 
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatDelegate
-import androidx.viewpager.widget.ViewPager
-import com.example.flam.HauptModels.AccountActivity
-import com.example.flam.HauptModels.ContactGoogleActivity
+import com.example.flam.HauptModels.PersonFragment
 import com.example.flam.HauptModels.ViewPagerAdapter
 import com.example.flam.databinding.ActivityHauptBinding
-import com.google.android.material.bottomnavigation.BottomNavigationView
+
 import kotlinx.android.synthetic.main.activity_haupt.*
 import java.util.ArrayList
 
 class HauptActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHauptBinding
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHauptBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
 
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         setUpTabbar()
@@ -35,27 +32,32 @@ class HauptActivity : AppCompatActivity() {
                 R.id.home -> {
                     viewpager.currentItem = 0
                     binding.textHaupt.text = "Home"
+                    binding.navbar.showBadge(R.id.home)
 
                 }
 
                 R.id.nav_contact -> {
+                    val contactFragment = ContactFragment()
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.framelayout, contactFragment).commit()
                     viewpager.currentItem = 1
                     binding.textHaupt.text = "Contact"
-                    val contactintent = Intent(this, ContactGoogleActivity::class.java)
-                    startActivity(contactintent)
 
                 }
                 R.id.nav_person -> {
+                    val personFragment = PersonFragment()
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.framelayout, personFragment).commit()
                     viewpager.currentItem = 2
                     binding.textHaupt.text = "Account"
-                    binding.navbar.showBadge(R.id.nav_settings)
+                    binding.navbar.showBadge(R.id.nav_person)
 
                 }
 
                 R.id.nav_settings -> {
                     viewpager.currentItem = 3
                     binding.textHaupt.text = "Settings"
-                    binding.navbar.showBadge(R.id.nav_settings)
+                    binding.navbar.showBadge(R.id.nav_settings,10000)
 
                 }
             }
@@ -70,8 +72,7 @@ class HauptActivity : AppCompatActivity() {
                 add("Settings")
             }
         }
+
     }
-
-
 
 }
