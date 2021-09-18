31f95f177b7dfcad1ffd571914301e65687781a3
Jekis11 <kogyxar94@gmail.com>
2021-09-18 15:22:44 +0300

    Add build

--- app/src/main/java/com/example/flam/HauptActivity.kt
+++ app/src/main/java/com/example/flam/HauptActivity.kt
@@ -1,10 +1,16 @@
 package com.example.flam
 
+
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatDelegate
+import com.example.flam.HauptModels.AccountActivity
+import com.example.flam.HauptModels.ContactGoogleActivity
 import com.example.flam.databinding.ActivityHauptBinding
 import com.example.flam.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_haupt.*
 
 class HauptActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHauptBinding
@@ -19,16 +25,21 @@ class HauptActivity : AppCompatActivity() {
         setUpTabbar()
     }
 
+
     private fun setUpTabbar() {
         binding.navbar.setOnItemSelectedListener {
             when(it) {
                 R.id.home -> binding.textHaupt.text = "Home"
                 R.id.nav_contact -> binding.textHaupt.text = "Contact"
                 R.id.nav_person -> {
+                    val intent = Intent(this,ContactGoogleActivity::class.java)
+                    startActivity(intent)
                     binding.textHaupt.text = "Account"
-                    binding.navbar.showBadge(R.id.nav_settings)
+                    binding.navbar.showBadge(R.id.nav_person)
                 }
                 R.id.nav_settings -> {
+                    val intent = Intent(this,AccountActivity::class.java)
+                    startActivity(intent)
                     binding.textHaupt.text = "Settings"
                     binding.navbar.dismissBadge(R.id.nav_settings)
                 }
@@ -36,4 +47,7 @@ class HauptActivity : AppCompatActivity() {
 
         }
     }
+
+
+
 }
