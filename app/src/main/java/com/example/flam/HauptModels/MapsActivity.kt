62d5a11d5c67b1ad43daba58c013b4b9a2a9e370
Jekis11 <kogyxar94@gmail.com>
2021-09-20 16:32:20 +0300

    Add build

--- app/src/main/java/com/example/flam/HauptModels/MapsActivity.kt
+++ app/src/main/java/com/example/flam/HauptModels/MapsActivity.kt
@@ -1,12 +1,10 @@
 package com.example.flam.HauptModels
 
-import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.flam.R
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
@@ -14,27 +12,24 @@ import com.google.android.gms.maps.model.MarkerOptions
 
 class MapsActivity : AppCompatActivity() {
 
-     lateinit var mapFragment: SupportMapFragment
-    lateinit var googleMap: GoogleMap
+    private lateinit var googleMap: GoogleMap
 
-    @SuppressLint("MissingPermission")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
 
-        mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(OnMapReadyCallback {
-            googleMap = it
-            googleMap.isMyLocationEnabled = true
-            val location1 = LatLng(13.03,77.60)
-            googleMap.addMarker(MarkerOptions().position(location1).title("My Location"))
-            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(location1,5f))
+        val mapFragment = supportFragmentManager
+                .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
 
+        fun onMapReady(googleMap: GoogleMap){
+            this.googleMap = googleMap
 
+            val andernach = LatLng(50.43109, 7.40425)
+            googleMap.addMarker(MarkerOptions().position(andernach).title("Andernach"))
+            googleMap.moveCamera(CameraUpdateFactory.newLatLng(andernach))
 
-
-
-        })
+        }
     }
 
 }
