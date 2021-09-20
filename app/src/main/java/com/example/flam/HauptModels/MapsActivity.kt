fa59f4f689ab07d9b37722220a156eb4bd27b96b
Jekis11 <kogyxar94@gmail.com>
2021-09-20 16:39:13 +0300

    Add build

--- app/src/main/java/com/example/flam/HauptModels/MapsActivity.kt
+++ app/src/main/java/com/example/flam/HauptModels/MapsActivity.kt
@@ -5,12 +5,13 @@ import android.os.Bundle
 import com.example.flam.R
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
 
-class MapsActivity : AppCompatActivity() {
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var googleMap: GoogleMap
 
@@ -21,15 +22,14 @@ class MapsActivity : AppCompatActivity() {
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+    }
 
-        fun onMapReady(googleMap: GoogleMap){
-            this.googleMap = googleMap
+    override fun onMapReady(p0: GoogleMap) {
+        this.googleMap = googleMap
 
-            val andernach = LatLng(50.43109, 7.40425)
-            googleMap.addMarker(MarkerOptions().position(andernach).title("Andernach"))
-            googleMap.moveCamera(CameraUpdateFactory.newLatLng(andernach))
-
-        }
+        val andernach = LatLng(50.43109, 7.40425)
+        googleMap.addMarker(MarkerOptions().position(andernach).title("Andernach"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(andernach))
     }
 
 }
