4d93b201cb76746a703c73735361a2db65655e7d
Jekis11 <kogyxar94@gmail.com>
2021-09-23 14:46:15 +0300

    Add build

--- app/src/main/java/com/example/flam/HauptModels/ContactFragment.kt
+++ app/src/main/java/com/example/flam/HauptModels/ContactFragment.kt
@@ -17,14 +17,6 @@ import kotlinx.android.synthetic.main.fragment_contact.*
 
 class ContactFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_contact,container,false)
-
-
-
-    }
-
-
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
