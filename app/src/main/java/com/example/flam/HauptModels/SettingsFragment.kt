fd71bba6c7071c4198d811b1354e6ad8f1379845
Jekis11 <kogyxar94@gmail.com>
2021-10-09 11:08:01 +0300

    Add build

--- app/src/main/java/com/example/flam/HauptModels/SettingsFragment.kt
+++ app/src/main/java/com/example/flam/HauptModels/SettingsFragment.kt
@@ -1,7 +1,33 @@
 package com.example.flam.HauptModels
 
+import android.app.Dialog
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.Window
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.flam.R
+import com.example.flam.databinding.FragmentPersonBinding
+import com.example.flam.models.User
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
 
 class SettingsFragment : Fragment(R.layout.fragment_settings) {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        val binding = FragmentPersonBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+    }
+
+}
 }
