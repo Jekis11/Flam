7b64936a5134ad6835c6b484b5100f0d2e33fafa
Jekis11 <kogyxar94@gmail.com>
2021-10-09 10:53:56 +0300

    Add build

--- app/src/main/java/com/example/flam/CategoryActivity.kt
+++ app/src/main/java/com/example/flam/CategoryActivity.kt
@@ -1,7 +1,9 @@
 package com.example.flam
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_category.*
 
 class CategoryActivity : AppCompatActivity() {
 
@@ -10,5 +12,11 @@ class CategoryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_category)
+
+        backpressed.setOnClickListener {
+            val intent = Intent(this,HauptActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
