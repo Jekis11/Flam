346db8a4a1a493c872727a7fda9b0a19fc1088e6
Jekis11 <kogyxar94@gmail.com>
2021-10-12 22:32:57 +0300

    Add build

--- app/src/main/java/com/example/flam/ViewAllActivity.kt
+++ app/src/main/java/com/example/flam/ViewAllActivity.kt
@@ -1,10 +1,16 @@
 package com.example.flam
 
+import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.flam.adapter.PopularAdapter
 import com.example.flam.adapter.ViewAllAdapter
+import com.example.flam.models.PopularModel
 import com.example.flam.models.ViewAll
+import com.facebook.internal.WebDialog
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
@@ -18,7 +24,21 @@ class ViewAllActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_all)
-
+        db = FirebaseFirestore.getInstance()
         val type = intent.getStringExtra("type")
+        viewAll = findViewById(R.id.view_all_rec)
+
+        viewAll.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL,false)
+        viewAllList = ArrayList()
+        viewAllAdapter = ViewAllAdapter(this, viewAllList as ArrayList<ViewAll>)
+        viewAll.adapter = viewAllAdapter
+
+
+        if(type != null && type.equals("keys")){
+
+          db.collection("AllProducts").whereEqualTo("type","keys").get()
+              .addOnCompleteListener {  }
+        }
+
     }
 }
