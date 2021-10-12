0bc75db035d54491589dde9889633e5618901ca3
Jekis11 <kogyxar94@gmail.com>
2021-10-12 14:19:29 +0300

    Add build

--- app/src/main/java/com/example/flam/adapter/ViewAllAdapter.kt
+++ app/src/main/java/com/example/flam/adapter/ViewAllAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.flam.adapter
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
@@ -10,10 +11,11 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.flam.R
 import com.example.flam.models.PopularModel
+import com.example.flam.models.ViewAll
 
 class ViewAllAdapter(
     private val context: Context,
-    private val popularModelList: List<PopularModel>
+    private val List: List<ViewAll>
 ) :
     RecyclerView.Adapter<ViewAllAdapter.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -22,24 +24,25 @@ class ViewAllAdapter(
         )
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        Glide.with(context).load(popularModelList[position].img_url).into(holder.popImg)
-        holder.name.text = popularModelList[position].name
-        holder.rating.text = popularModelList[position].rating
-        holder.description.text = popularModelList[position].description
-        holder.discount.text = popularModelList[position].discount
+        Glide.with(context).load(List[position].img_url).into(holder.imageview)
+        holder.name.text = List[position].name
+        holder.rating.text = List[position].rating
+        holder.description.text =List[position].description
+        holder.price.text = List[position].price +"€"
     }
 
     override fun getItemCount(): Int {
-        return popularModelList.size
+        return List.size
     }
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        var popImg: ImageView = itemView.findViewById(R.id.pop_img)
-        var name: TextView = itemView.findViewById(R.id.nameproducts)
-        var description: TextView = itemView.findViewById(R.id.pop_desc)
-        var rating: TextView = itemView.findViewById(R.id.rating)
-        var discount: TextView = itemView.findViewById(R.id.pop_discount)
+        var imageview: ImageView = itemView.findViewById(R.id.imageview)
+        var name: TextView = itemView.findViewById(R.id.nav_cat_name)
+        var description: TextView = itemView.findViewById(R.id.nav_cat_des)
+        var rating: TextView = itemView.findViewById(R.id.rec_rating)
+        var price: TextView = itemView.findViewById(R.id.pricenaveuro)
 
     }
 }
