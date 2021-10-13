e78600ad816b00f6132f5ee39db4ab26e581dec0
Jekis11 <kogyxar94@gmail.com>
2021-10-13 16:35:00 +0300

    Add build

--- app/src/main/java/com/example/flam/RateUs.kt
+++ app/src/main/java/com/example/flam/RateUs.kt
@@ -1,48 +1,20 @@
 package com.example.flam
 
-import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-
-import com.example.flam.databinding.ActivityRateUsBinding
-import kotlinx.android.synthetic.main.activity_rate_us.*
+import dev.yuganshtyagi.smileyrating.SmileyRatingView
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.rateus.*
 
 class RateUs : AppCompatActivity() {
-        // use getter to get non-null value for binding variable
-        private var _binding: ActivityRateUsBinding? = null
-        private val binding get() = _binding!!
 
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.rateus)
 
-            _binding = ActivityRateUsBinding.inflate(layoutInflater)
-            setContentView(binding.root)
-
-            captureRating()
-        }
-
-        @SuppressLint("SetTextI18n") // ignore concatenation warning
-        private fun captureRating() {
-            var userRating = 0F // initial value
-
-            // use _ for unused parameters in this lambda function
-            binding.ratingBar.apply {
-                setOnRatingBarChangeListener { _, rating, _ ->
-                    userRating = rating.also {
-                        binding.tvRating.text = "${it.toInt()}/5"
-                    }
-                }
-            }
-
-
-            binding.btnSend.setOnClickListener {
-                if (binding.etRating.text!!.isNotEmpty()) {
-                    Toast.makeText(this, "Rating ($userRating/5.0) sent to Oigen", Toast.LENGTH_SHORT)
-                        .show()
-                } else {
-                    binding.etRating.error = "can't be blank"
-                }
-            }
+        rating_bar.setOnRatingBarChangeListener { _, rating, _ ->
+            smiley_view.setSmiley(rating = rating)
         }
     }
+}
