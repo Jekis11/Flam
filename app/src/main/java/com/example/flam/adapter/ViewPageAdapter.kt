61eb1488a9f4363c25e419832b986a2c8cbf3125
Jekis11 <kogyxar94@gmail.com>
2021-10-19 14:00:34 +0300

    Add build

--- app/src/main/java/com/example/flam/adapter/ViewPageAdapter.kt
+++ app/src/main/java/com/example/flam/adapter/ViewPageAdapter.kt
@@ -1,4 +1,27 @@
 package com.example.flam.adapter
 
-class ViewPageAdapter {
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.flam.R
+import com.example.flam.adapter.ViewPageAdapter.*
+
+class ViewPageAdapter(private var image: List<Int>): RecyclerView.Adapter<Pager2ViewHolder>() {
+
+    inner class Pager2ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val itemImage: ImageView = itemView.findViewById(R.id.detail_img)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Pager2ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: Pager2ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
 }
