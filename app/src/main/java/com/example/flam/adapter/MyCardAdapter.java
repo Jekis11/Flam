e2ef091bf4437bc8e8e85b4ba025611a9a59e7be
Jekis11 <kogyxar94@gmail.com>
2022-02-02 12:11:51 +0200

    Hallo Oigen

--- app/src/main/java/com/example/flam/adapter/MyCardAdapter.java
+++ app/src/main/java/com/example/flam/adapter/MyCardAdapter.java
@@ -1,18 +1,34 @@
 package com.example.flam.adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.flam.R;
+import com.example.flam.models.MyCartModels;
+
+import java.util.List;
+
 public  class MyCardAdapter  extends RecyclerView.Adapter<MyCardAdapter.ViewHolder> {
 
+    Context context;
+    List<MyCartModels> cartModelsList;
+
+    public MyCardAdapter(Context context, List<MyCartModels> cartModelsList) {
+        this.context = context;
+        this.cartModelsList = cartModelsList;
+    }
 
     @NonNull
     @Override
     public MyCardAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.my_cart_item,parent,false));
+
     }
 
     @Override
@@ -26,8 +42,20 @@ public  class MyCardAdapter  extends RecyclerView.Adapter<MyCardAdapter.ViewHold
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView name, price, data, time, quantity, totalPrice;
+
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            name = itemView.findViewById(R.id.productname);
+            price = itemView.findViewById(R.id.productprice);
+            data = itemView.findViewById(R.id.currentdate);
+            time = itemView.findViewById(R.id.currenttime);
+            quantity = itemView.findViewById(R.id.total_Quantity);
+            totalPrice = itemView.findViewById(R.id.totalPrice);
+
         }
     }
 }
