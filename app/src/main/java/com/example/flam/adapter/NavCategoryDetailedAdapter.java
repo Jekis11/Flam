5582df287d1cf103c4a0363fa2edd0319db42eec
Jekis11 <kogyxar94@gmail.com>
2022-02-05 19:04:48 +0200

    Hallo Oigen

---
+++ app/src/main/java/com/example/flam/adapter/NavCategoryDetailedAdapter.java
@@ -0,0 +1,55 @@
+package com.example.flam.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.flam.R;
+import com.example.flam.models.NavCategoryDetailedModel;
+
+import java.util.List;
+
+public class NavCategoryDetailedAdapter extends RecyclerView.Adapter<NavCategoryDetailedAdapter.ViewHolder> {
+
+    Context context;
+    List<NavCategoryDetailedModel> list;
+
+    public NavCategoryDetailedAdapter(Context context, List<NavCategoryDetailedModel> list) {
+        this.context = context;
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public NavCategoryDetailedAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.nav_category_detailed_item,parent,false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NavCategoryDetailedAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageview;
+        TextView name, price;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            imageview = itemView.findViewById(R.id.imageview);
+            name = itemView.findViewById(R.id.nav_cat_name);
+            price = itemView.findViewById(R.id.price);
+        }
+    }
+}
