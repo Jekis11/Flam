2fc8d0f137cd8bc5940293c672391653ed5ecc52
Jekis11 <kogyxar94@gmail.com>
2022-02-06 15:10:17 +0200

    Hallo Oigen

---
+++ app/src/main/java/com/example/flam/HauptModels/NewProductsActivity.java
@@ -0,0 +1,14 @@
+package com.example.flam.HauptModels;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewProductsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_products);
+    }
+}
