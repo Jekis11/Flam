b1ace08929690fc0762b2b00fcb1f2c5684425a3
Jekis11 <kogyxar94@gmail.com>
2022-02-06 20:54:31 +0200

    Hallo Oigen

--- app/src/main/java/com/example/flam/HauptModels/CartActivity.java
+++ app/src/main/java/com/example/flam/HauptModels/CartActivity.java
@@ -1,16 +1,67 @@
 package com.example.flam.HauptModels;
 
+import static android.app.PendingIntent.getActivity;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import com.example.flam.R;
+import com.example.flam.adapter.MyCardAdapter;
+import com.example.flam.models.MyCartModels;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CartActivity extends AppCompatActivity {
+
+
+    FirebaseFirestore db;
+    FirebaseAuth auth;
+    RecyclerView recyclerView;
+    MyCardAdapter cardAdapter;
+    List<MyCartModels> cartModelsList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
+
+        db = FirebaseFirestore.getInstance();
+        auth = FirebaseAuth.getInstance();
+        recyclerView = findViewById(R.id.recyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        cartModelsList = new ArrayList<>();
+        cardAdapter = new MyCardAdapter(this,cartModelsList);
+        recyclerView.setAdapter(cardAdapter);
+
+        db.collection("AddToCart").document(auth.getCurrentUser().getUid())
+                .collection("CurrentUser").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+
+                if(task.isSuccessful()){
+                    for (DocumentSnapshot documentShapshot : task.getResult().getDocuments()){
+                        MyCartModels cartModels = documentShapshot.toObject(MyCartModels.class);
+                        cartModelsList.add(cartModels);
+                        cardAdapter.notifyDataSetChanged();
+                    }
+                }
+            }
+        });
+
+
+
+
     }
 }
